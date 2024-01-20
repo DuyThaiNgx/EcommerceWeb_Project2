46781392ebd7b9fd9787d3a7e743b6b4bd718bf7
Duy Thai <sb23092002@gmail.com>
2024-01-21 04:02:20 +0700

    fix chuc nang DoiMK

--- src/main/java/com/selena/controller/administrator/AdminChangePasswordController.java
+++ src/main/java/com/selena/controller/administrator/AdminChangePasswordController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -24,7 +25,7 @@ public class AdminChangePasswordController extends BaseController {
     public AdminChangePasswordController(UserService userService) {
         this.userService = userService;
     }
-
+    // Hiển thị giao diện đổi mật khẩu
     @GetMapping("/admin/change-password")
     public String showChangePasswordPage(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -34,9 +35,34 @@ public class AdminChangePasswordController extends BaseController {
         return "administrator/change_password";
     }
 
+    //    @PostMapping("/admin/change-password")
+//    public String processChangePassword(
+//            @ModelAttribute("passwordForm") ChangePasswordForm passwordForm,
+//            RedirectAttributes redirectAttributes,
+//            Model model) {
+//
+//        User user = userService.getCurrentUser();
+//
+//        if (user != null && passwordEncoder.matches(passwordForm.getCurrentPassword(), user.getPassword())) {
+//            if (passwordForm.getNewPassword().equals(passwordForm.getConfirmPassword())) {
+//                // Update password logic with encoding
+//                user.setPassword(passwordEncoder.encode(passwordForm.getNewPassword()));
+//                userService.save(user);
+//                redirectAttributes.addFlashAttribute("successMessage", "Password changed successfully!");
+//                return "redirect:/admin/change-password";
+//            } else {
+//                model.addAttribute("error", "New password does not match confirm password.");
+//            }
+//        } else {
+//            model.addAttribute("error", "Incorrect current password.");
+//        }
+//        return "administrator/change_password";
+//    }
+    //Thao tác với việc thay đổi mật khẩu
     @PostMapping("/admin/change-password")
     public String processChangePassword(
             @ModelAttribute("passwordForm") ChangePasswordForm passwordForm,
+            BindingResult bindingResult,
             RedirectAttributes redirectAttributes,
             Model model) {
 
@@ -44,17 +70,21 @@ public class AdminChangePasswordController extends BaseController {
 
         if (user != null && passwordEncoder.matches(passwordForm.getCurrentPassword(), user.getPassword())) {
             if (passwordForm.getNewPassword().equals(passwordForm.getConfirmPassword())) {
-                // Update password logic with encoding
+                // Để cập nhật mật khẩu với mã hóa
                 user.setPassword(passwordEncoder.encode(passwordForm.getNewPassword()));
                 userService.save(user);
-                redirectAttributes.addFlashAttribute("successMessage", "Password changed successfully!");
-                return "redirect:/admin/change-password";
+//                redirectAttributes.addFlashAttribute("successMessage", "Password changed successfully!");
+                return "redirect:/admin/change-password?successPasswordChange=true";
             } else {
-                model.addAttribute("error", "New password does not match confirm password.");
+//                bindingResult.rejectValue("confirmPassword", "error.user", "New password does not match confirm password.");
+                return "redirect:/admin/change-password?confirmPassword_error=true";
             }
         } else {
-            model.addAttribute("error", "Incorrect current password.");
+//            bindingResult.rejectValue("currentPassword", "error.user", "Incorrect current password.");
+            redirectAttributes.addFlashAttribute("changepw_error", "true");
+            return "redirect:/admin/change-password?changepw_error=true";
         }
-        return "administrator/change_password";
+//        return "administrator/change_password";
     }
+
 }
